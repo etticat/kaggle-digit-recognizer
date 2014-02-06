3cbb8c05ce882913f0d05ae564dbff33f86351e4
Michael Ettlinger <michael@etticat.net>
2014-02-06 21:20:40 +0100

    Merged digit-showing changes

--- src_nn1/showDigit.m
+++ src_nn1/showDigit.m
@@ -1,6 +1,20 @@
 function showDigit(digit)
 
-imagesc(squeeze(digit), [0 255]); 
-colormap(gray)
+ ran_show = randi(42000, [25,1]);
+ count=1;
+ show_arr = digit(:,:,ran_show(1));
+ 
+ for i=1:5
+     show_arr = digit(:,:,ran_show(count));
+     for j=1:4         
+         count = count+1;
+         show_arr = [show_arr,digit(:,:,ran_show(count))];
+     end
+     
+         subplot(5,1,i);
+         imshow(show_arr);     
+ end
+    
+ pause;
 
 end
