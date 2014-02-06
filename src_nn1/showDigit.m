9ba84e3545c6868484eb9d45b0d491b9fc140762
Michael Ettlinger <michael@etticat.net>
2014-02-06 22:15:38 +0100

    Reverted showDigit changes

--- src_nn1/showDigit.m
+++ src_nn1/showDigit.m
@@ -1,20 +1,6 @@
 function showDigit(digit)
 
- ran_show = randi(42000, [25,1]);
- count=1;
- show_arr = digit(:,:,ran_show(1));
- 
- for i=1:5
-     show_arr = digit(:,:,ran_show(count));
-     for j=1:4         
-         count = count+1;
-         show_arr = [show_arr,digit(:,:,ran_show(count))];
-     end
-     
-         subplot(5,1,i);
-         imshow(show_arr);     
- end
-    
- pause;
+-imagesc(squeeze(digit), [0 255]); 
+-colormap(gray);
 
 end
